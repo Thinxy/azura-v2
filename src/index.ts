cf3a574f376d05645fe4e8d8602b94fffdf6c40a
Wyntch <122948478+Thinxy@users.noreply.github.com>
2025-03-09 18:18:31 -0300

    Azura: exportando components utilizavéis pra usuários

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export { AzuraServer } from "./AzuraServer";
+export { Request, Response } from "./types";
+export { Plugin, RouterHandler, Middleware, RouteMeta } from "./types";
+export { RedirectExtensions } from "./core/extensions/redirect";
+export { GetExtensions } from "./core/extensions/get";
+export { PostExtensions } from "./core/extensions/post";
+export { PutExtensions } from "./core/extensions/put";
+export { DeleteExtensions } from "./core/extensions/delete";
+export { ServerOptions } from "./types";
